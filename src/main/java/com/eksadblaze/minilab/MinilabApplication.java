6f22afd623ed8498574c4d53547deddf49bc8cb8
Kemal Adam Roisy <kemal.roisy@gmail.com>
2022-04-04 00:56:36 +0700

    initialize project from spring.io

---
+++ src/main/java/com/eksadblaze/minilab/MinilabApplication.java
@@ -0,0 +1,13 @@
+package com.eksadblaze.minilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinilabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinilabApplication.class, args);
+	}
+
+}
